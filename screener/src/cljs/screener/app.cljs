df4d0c9dd133be5f0480823fe37f238fef8b31f2
dviramontes <dviramontes@gmail.com>
2015-05-12 11:34:48 -0600

    begins question fn

--- screener/src/cljs/screener/app.cljs
+++ screener/src/cljs/screener/app.cljs
@@ -4,18 +4,27 @@
 
 (enable-console-print!) ;; enable print at web inspector console)
 
-(defn some-component []
+(defn question [text subtext type id]
   [:div.row
-   [:h3 "I am a component!"]
-   [:p.someclass
-    "I have " [:strong "bold"]
-    [:span {:style {:color "#FF306D"}} " and red"]
-    " text."]])
+   [:div.col-lg-12
+      [:h3 text]
+      [:input.form-control {:field type :id id}]
+      [:p.lead "subtext"]]])
 
-(defn calling-component []
-  [:div.container "Parent component"
-   [some-component]])
+(defn parent-component []
+  [:div.container 
+    [:div.row
+      [:div.col-lg-12
+        [:p.lead 
+          [:b "> "]
+          "Please tell us about your project."]]]
+    [question 
+      "Is this a re-design or a new project?"
+    :text :one]
+    [question 
+      "What are the goals for this project?"
+    :text :one]])
 
 (defn init []
-  (reagent/render-component [calling-component]
+  (reagent/render-component [parent-component]
                             (.getElementById js/document "mount")))
