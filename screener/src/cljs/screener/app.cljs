1c9f5303fc74f6c75c125ad63e9f4b3c5c0a818c
david viramontes <dviramontes@gmail.com>
2015-05-22 22:17:02 -0600

    textareas instead of input:text makes more sense

--- screener/src/cljs/screener/app.cljs
+++ screener/src/cljs/screener/app.cljs
@@ -19,7 +19,7 @@
 (defn submit-fn [e]
   (POST "http://localhost:4000/email-form"
        {:handler handler
-        :params {:data "foo bar baz" }
+        :params {:data "kdmaslkmdals"}
         :error-handler error-handler})
   (print "clicked"))
 
@@ -34,29 +34,29 @@
             :date-format "yyyy/mm/dd"
             :inline true}]]]]]))
 
-(defn number [title subtext id]
+(defn number [title subtext id opts]
   [:div.row
    [:div.col-lg-12
     [:h3 title]
-    [:label [:em.subtext subtext]
-     [:input.form-control {:field :numeric :id id}]]]])
+    [:label [:em.help-block subtext]
+     [:input.form-control {:field :numeric :id id :placeholder opts}]]]])
 
-(defn text [title subtext id]
+(defn text [title subtext id opts]
   [:div.row
-   [:div.col-lg-12
+   [:div.col-lg-8
     [:h3 title]
-    [:label [:em.subtext subtext]
-     [:input.form-control {:field :text :id id}]]]])
+    [:label [:em.help-block subtext]]
+    [:textarea.form-control {:rows 3 :field :text :id id :placeholder opts}]]])
 
 (defn email [title subtext id]
   [:div.row
    [:div.col-lg-12
     [:h3 title]
-    [:label [:em.subtext subtext]
+    [:label [:em.help-block.help-block subtext]
      [:input.form-control {:field :email :id id :placeholder "me@mail.com"}]]]])
 
 (defn selection-box [title subtext id values]
-  (print values)
+  ;; (print values)
   [:div.row
    [:div.col-lg-12
     [:h3 title]
@@ -70,7 +70,7 @@
     [:h3 title]
     [:div.form-group
      [:label subtext]
-     [:select.form-control {:field :list :id id}
+     [:select.form-control {:field :list :id :many.options}
       (for [opt options]
         [:option {:key opt} (name opt)])]]]])
 
@@ -78,8 +78,8 @@
   (case type
     :selection-box (selection-box title subtext id options)
     :list  (lista title subtext id options)
-    :text  (text  title subtext id)
-    :numeric (number title subtext id)
+    :text  (text  title subtext id options)
+    :numeric (number title subtext id options)
     :email   (email title subtext id)
     ;;:datepicker (datepicker title id)
     ))
@@ -119,7 +119,7 @@
     (question
      "When does our work need to be finished?"
      "What is your target total completion date? What is driving that?"
-     :text :six)
+     :text :six "Q1, Q3, 2015, 2016")
 
     ; (question
     ;  "What is most important about this project?"
@@ -145,13 +145,13 @@
 
     (question
      "What are you worried about?"
-     "What do you imagine going wrong? Don't worry, you're in good company"
+     "What do you imagine going wrong? Don't worry, you are in good company"
      :text :eleven)
 
     (question
      "What is your budget range?"
-     "$$ amount , ballpark figure is fine"
-     :numeric :twelve)
+     "ballpark figure is fine"
+     :numeric :twelve "$")
 
     (question
      "What does the selection process look like on your end?"
@@ -159,7 +159,7 @@
      to be making a decision?"
      :text :thirteen)
 
-    (question "Lastly, Where can we contact you?" nil :email :email)
+    (question "Lastly, how do we get in touch with you?" nil :email :email)
 
     [:br]
 
@@ -179,12 +179,13 @@
      [:div.row
       [:div.col-lg-12
        [:br]
-       [:p.lead "Tell us about your project"]]
+       [:p.lead {:style {:font-size 30}}
+        [:b "> "]
+        "Tell us about your project"]]
       [:div.col-lg-6.col-md-12.col-sm.12
        [:div.container
-          [bind-fields form doc (fn [id value doc;;{:keys [email] :as doc}
-            ]
-             (print doc))]]]]]))
+        ;;{:keys [email] :as doc}
+        [bind-fields form doc (fn [id value doc] (print doc))]]]]]))
 
 (defn init []
   (r/render-component [parent-component]
